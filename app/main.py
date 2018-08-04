c93eb22abcdcf24d662eb9bdd49d01a6358a7292
Beruto <beruto000@gmail.com>
2018-08-04 12:40:30 +0200

    updated requirements, SQL 3 tables and the query-inserto works well

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 import os
 from sys import argv
-from flask_cors import CORS
+
 from Services import space
 from flask import jsonify, Flask
 from flask import request
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
 from handler import HandlerTest
 from psutil import cpu_percent
 from slack_sender import SlackSender
@@ -22,9 +24,37 @@ else:
 
 # Flask APP
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://Beruto:20911332p@localhost/raspberry-ci'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+db = SQLAlchemy(app)
 
 slack_sender = SlackSender(SLACK_WEBHOOK)
 handler_test = HandlerTest(slack_sender)
+
+
+# ============ Models ============
+class UsersModel(db.Model):
+    username = db.Column(db.String(250), primary_key=True)
+    name = db.Column(db.String(250))
+    avatar_url = db.Column(db.String(500))
+
+
+class ProjectsModel(db.Model):
+    project_name = db.Column(db.String(250), primary_key=True)
+    project_url = db.Column(db.String(500))
+
+
+history_push = db.Table('history_push', db.Column('updated_on', db.DateTime(), primary_key=True),  # updated_on
+                        db.Column('username', db.String(250), db.ForeignKey('users_model.username'), primary_key=True),
+                        # username FKdb.relationship('Tag', secondary=tags, lazy='subquery',
+                        db.Column('project_name', db.String(250), db.ForeignKey('projects_model.project_name'),
+                                  nullable=False),
+                        # project_name FK
+                        db.Column('build_passed', db.Boolean(), nullable=True)  # build_passed
+                        )
+
+
+# ============ END Models ============
 
 
 @app.route("/java/maven", methods=["POST"])
@@ -57,10 +87,17 @@ def get_space():
 
 @app.route("/api/cpu_info")
 def cpu_info():
+    # statement = history_push.insert().values(updated_on=datetime.now(),
+    #  username='beruto', project_name='Demo', build_passed=True)
+    # db.session.execute(statement)
+    # db.session.commit()
+    # print([x for x in db.session.execute(history_push.select())])
+
     return jsonify({"cpu": cpu_percent()})
 
 
 # Main
 if __name__ == '__main__':
     CORS(app)
+    db.create_all()
     app.run(host='0.0.0.0', debug=True)
